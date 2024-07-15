ee11fb7bf4ec2e3e55b23ab05d7808c3e84621c3
John Dykstra <john.dykstra1@gmail.com>
2024-07-15 12:36:44 -0500

    Create repo.

---
+++ env.sh
@@ -0,0 +1,10 @@
+# Define environment variables needed across multiple scripts.  This file must be sourced, not executed.
+
+# Root for music server.
+MUSIC_ROOT=`pwd`
+
+# Python virtual environment for Quod Libet.
+QUODLIBET_ENVIRONMENT=quodlibet_venv
+
+# Where everything associated with CamillaDSP lives.
+CAMILLADSP_DIR=camilladsp
